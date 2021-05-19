2c7b9d45bf8476296d7d527f3bca874c8673f914
qiaozp <qiaozp@B-MAA4ML7H-2021.local>
2021-05-19 09:21:58 +0800

    trial and fix issue, now need to fix autojump and completion

--- macOS/migrate.sh
+++ macOS/migrate.sh
@@ -4,8 +4,10 @@ REPODIR=~/repo
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s .vimrc ~/.vimrc
-ln -s .zshrc ~/.zshrc
+rm ~/.vimrc
+rm ~/.zshrc
+ln -sF $DOTFILEDIR/.vimrc ~/.vimrc
+ln -sF $DOTFILEDIR/.zshrc ~/.zshrc
 cp .ideavimrc ~
 
 #proxy
@@ -17,6 +19,7 @@ brew install cowsay
 brew install zsh-syntax-highlighting
 
 #install omz and plugin
+rm -rf $HOME/.oh-my-zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
