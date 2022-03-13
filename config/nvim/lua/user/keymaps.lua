e8fe4893508f0455ae3c162277948e30b3f29268
qiaozp <chivalry.pp@gmail.com>
2022-03-13 16:44:13 +0800

    Remove overlapped mappings

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -20,8 +20,6 @@ keymap("n", "yL", "y$", opts)
 keymap("n", "yH", "y^", opts)
 
 -- move idea from hrsh7th
-keymap("n", "<C-h>", "<C-o>zz", opts)
-keymap("n", "<C-l>", "<C-i>zz", opts)
 keymap("i", "<C-h>", "<Left>", opts)
 keymap("i", "<C-l>", "<Right>", opts)
 
