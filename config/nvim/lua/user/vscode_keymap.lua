59118c45d075cca36c4c808be6f47ba156cd0109
Qiao <chivalry.pp@gmail.com>
2024-11-13 11:00:02 +0800

    Add basic vscode neovim keybinding
    Signed-off-by: Qiao <chivalry.pp@gmail.com>

--- config/nvim/lua/user/vscode_keymap.lua
+++ config/nvim/lua/user/vscode_keymap.lua
@@ -27,7 +27,17 @@ keymap("n", "gj", "j", opts)
 keymap("n", "-", "ddp", opts)
 keymap("n", "_", "ddkP", opts)
 
+keymap("n", "gs", function() vscode.action("workbench.files.action.focusFilesExplorer") end, opts)
+
 local vscode = require('vscode')
 keymap("n", "<leader>rn", function() vscode.action("editor.action.rename") end, opts)
-keymap("n", "<leader>t", function() vscode.action("workbench.action.terminal.toggleTerminal") end, opts)
 
+
+-- debug related
+keymap("n", "<leader>de", function() vscode.action("workbench.action.debug.start") end, opts)
+keymap("n", "<leader>x", function() vscode.action("workbench.action.debug.stop") end, opts)
+keymap("n", "<leader>g", function() vscode.action("workbench.action.debug.continue") end, opts)
+keymap("n", "<leader>o", function() vscode.action("workbench.action.debug.stepOver") end, opts)
+keymap("n", "<leader>i", function() vscode.action("workbench.action.debug.stepInto") end, opts)
+keymap("n", "<leader>t", function() vscode.action("workbench.action.debug.stepOut") end, opts)
+keymap("n", "<leader>b", function() vscode.action("editor.debug.action.toggleBreakpoint") end, opts)
